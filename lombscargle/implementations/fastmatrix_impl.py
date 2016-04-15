fix normalization in matrix methods

lombscargle/implementations/fastmatrix_impl.py
@@ -122,7 +122,7 @@ def lombscargle_fastmatrix(t, y, dy, frequency, normalization='normalized',
     p = np.array([compute_power(i) for i in range(Nf)])
 
     if normalization == 'unnormalized':
-        p *= 0.5
+        p *= 0.5 * t.size / ws
     elif normalization == 'normalized':
         p /= chi2_ref
     else:

lombscargle/implementations/matrix_impl.py
@@ -72,7 +72,7 @@ def lombscargle_matrix(t, y, dy, frequency, normalization='normalized',
     p = np.array([compute_power(f) for f in frequency])
 
     if normalization == 'unnormalized':
-        p *= 0.5
+        p *= 0.5 * t.size / (dy ** -2).sum()
     elif normalization == 'normalized':
         p /= chi2_ref
     else:

lombscargle/implementations/tests/test_lombscargle_methods.py
@@ -38,15 +38,16 @@ def data(N=100, period=1, theta=[10, 2, 3], dy=1, rseed=0):
 def test_lombscargle_methods_common(lombscargle_method, center_data,
                                     normalization, data):
     t, y, dy = data
+    dy = 1
     freq = 0.8 + 0.01 * np.arange(40)
 
     kwds = dict(normalization=normalization, center_data=center_data)
 
-    expected_output = lombscargle_slow(t, y, dy=np.ones_like(t),
+    expected_output = lombscargle_slow(t, y, dy,
                                        frequency=freq,
                                        fit_bias=False, **kwds)
 
-    output = lombscargle_method(t, y, dy=None, frequency=freq, **kwds)
+    output = lombscargle_method(t, y, dy, frequency=freq, **kwds)
     assert_allclose(output, expected_output)
 
 
@@ -56,17 +57,17 @@ def test_lombscargle_methods_common(lombscargle_method, center_data,
 @pytest.mark.parametrize('normalization', ['normalized', 'unnormalized'])
 def test_lombscargle_methods_with_bias(lombscargle_method, center_data,
                                        fit_bias, normalization, data):
-       t, y, freq = data
+       t, y, dy = data
        freq = 0.8 + 0.01 * np.arange(40)
 
        kwds = dict(normalization=normalization, center_data=center_data,
                    fit_bias=fit_bias)
 
-       expected_output = lombscargle_slow(t, y, dy=np.ones_like(t),
+       expected_output = lombscargle_slow(t, y, dy,
                                           frequency=freq,
                                           **kwds)
 
-       output = lombscargle_method(t, y, dy=None, frequency=freq, **kwds)
+       output = lombscargle_method(t, y, dy, frequency=freq, **kwds)
        assert_allclose(output, expected_output)
 
 
@@ -81,14 +82,14 @@ def test_lombscargle_methods_with_nterms(lombscargle_method, nterms,
     if nterms == 0 and not fit_bias:
         return
 
-    t, y, freq = data
+    t, y, dy = data
     freq = 0.8 + 0.01 * np.arange(40)
 
     kwds = dict(center_data=center_data, fit_bias=fit_bias,
                 normalization=normalization, nterms=nterms)
 
-    expected_output = lombscargle_matrix(t, y, dy=np.ones_like(t),
+    expected_output = lombscargle_matrix(t, y, dy,
                                          frequency=freq, **kwds)
 
-    output = lombscargle_method(t, y, dy=None, frequency=freq, **kwds)
+    output = lombscargle_method(t, y, dy, frequency=freq, **kwds)
     assert_allclose(output, expected_output, atol=1E-20)